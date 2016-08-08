55ba3141db109961faead83fca165b57b45ed7bb
yamadapc <tacla.yamada@gmail.com>
2016-08-08 14:51:40 -0300

    Updates for standalone executable

--- src/Hpack/Convert/Run.hs
+++ src/Hpack/Convert/Run.hs
@@ -21,16 +21,15 @@ runConvert :: IO ()
 runConvert = do
     as <- getArgs
     (dir, cabalFileFP) <- case as of
-        ("--convert":dir:_) -> do
+        (dir:_) -> do
             isFile <- doesFileExist dir
             if takeExtension dir == ".cabal" && isFile
                 then return (takeDirectory dir, dir)
                 else (dir,) <$> findCabalFileFP dir
-        ["--convert"] -> do
+        _ -> do
             cwd <- getCurrentDirectory
             cabalFileFP <- findCabalFileFP cwd
             return (cwd, cabalFileFP)
-        _ -> die "Usage: hpack --convert [ dir | cabal file ]"
     pkg <- runConvert' dir cabalFileFP
     writePackage (dir </> "package.yaml") pkg
     putStrLn $ "generated package.yaml based on " ++ cabalFileFP
